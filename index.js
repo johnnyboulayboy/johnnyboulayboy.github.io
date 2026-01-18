dd4fdf1642a9456e65ea19e7b68303dad0d6f25e
JohnnyBoulayBoy (J.B.B) <95968300+johnnyboulayboy@users.noreply.github.com>
2026-01-18 22:59:49 +0000

    updating the website lol

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 let click = 0
+let theme = 0 
+if (localStorage.getItem("Theme") == 1) {
+    theme = 1 // Dark mode
+    document.getElementById("Text1").style.color = "#FFFFFF";
+    document.body.style.backgroundColor = "#000000"
+} else {
+    theme = 0 // Light mode - Default
+    document.getElementById("Text1").style.color = "#000000";
+    document.body.style.backgroundColor = "#FFFFFF"
+}
+
 function clicks() {
     click = click + 1;
     document.getElementById("BUTTon").innerText = click;
@@ -18,4 +29,17 @@ function clicks() {
         alert("Bro what.")
     }
 
+}
+
+function changetheme() {
+    if (theme == 0) {
+        theme = 1;
+        document.getElementById("Text1").style.color = "#FFFFFF";
+        document.body.style.backgroundColor = "#000000"
+    } else {
+        theme = 0;
+        document.getElementById("Text1").style.color = "#000000";
+        document.body.style.backgroundColor = "#FFFFFF"
+    }
+    localStorage.setItem("Theme", theme);
 }
