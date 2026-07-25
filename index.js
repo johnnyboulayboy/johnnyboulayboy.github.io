9d6e8f1bff0596f0bf8be6ad307649d53e9d176c
JohnnyBoulayBoy <johnnyboulay655@gmail.com>
2026-07-25 12:54:23 +0200

    putting the website on maintenance.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let click = 0
 let theme = 0 
 if (localStorage.getItem("Theme") == 1) {
     theme = 1 // Dark mode
@@ -7,6 +8,27 @@ if (localStorage.getItem("Theme") == 1) {
     theme = 0 // Light mode - Default
     document.getElementById("Text1").style.color = "#000000";
     document.body.style.backgroundColor = "#FFFFFF"
+}
+
+function clicks() {
+    click = click + 1;
+    document.getElementById("BUTTon").innerText = click;
+
+    if (click == 20) {
+        alert("wow... huh... why.")
+    } else if (click == 50) {
+        alert("Bro. There's is no easter egg.")
+    } else if (click == 100) {
+        alert("Oh come on dude. Why are you even doing this.")
+    } else if (click == 150) {
+        alert("Stop or at 200 I remove the button\nYou button clicker weirdo.")
+    } else if (click == 200) {
+        document.getElementById("BUTTon").remove()
+        alert("OH LOOK THE BUTTON MISTERIOUSLY DISAPEARED")
+    } else if (click == 201) {
+        alert("Bro what.")
+    }
+
 }
 
 function changetheme() {
@@ -20,8 +42,4 @@ function changetheme() {
         document.body.style.backgroundColor = "#FFFFFF"
     }
     localStorage.setItem("Theme", theme);
-}
-
-function loadpage(page) {
-    window.location.href= page;
 }
